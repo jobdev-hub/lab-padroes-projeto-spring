4adb74ea19e23f937b87ae808f9f3d769ada52a4
jobdev-hub <job.methods@outlook.com>
2022-08-15 22:08:58 -0300

    adicionado lombok

--- src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -1,43 +1,26 @@
 package one.digitalinnovation.gof.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@Getter
+@Setter
 @Entity
 public class Cliente {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String nome;
-	@ManyToOne
-	private Endereco endereco;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	public Long getId() {
-		return id;
-	}
+    private String nome;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
+    @ManyToOne
+    private Endereco endereco;
 
 }
